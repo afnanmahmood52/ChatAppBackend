71c05b028d101ef3b7f77ab90a7c7a6177aca46f
Afnan Mahmood <afnanmahmood52@gmail.com>
2023-05-13 17:52:37 +0500

    Initial Commit

---
+++ Configurations/SocketEvents.js
@@ -0,0 +1,7 @@
+export const EventNames ={
+    Connect:'connect',
+    Disconnect:'disconnect',
+    JoinRoom:'join-room',
+    SendMessage:'send-message',
+    ReceiveMessage:'receive-message',
+}
